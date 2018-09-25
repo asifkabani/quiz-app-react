bc55f15a284fb6b9bd04d47b43dd6ee159f8fefe
Asif Kabani <asifkabani@gmail.com>
2018-09-24 23:32:54 -0500

    Updates to validation

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,15 @@ import quizQuestions from '../components/api/questions.json';
 import QuestionCount from '../components/QuestionCount';
 import ScoreCount from '../components/ScoreCount';
 import Content from '../components/Content';
-import { randomize } from '../components/Helpers';
+import { randomize, single } from '../components/Helpers';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            intro: true,
-            score: 0,
-            question: 0,
+            isIntro: true,
+            userScore: 0,
+            questionCount: 0,
             quizQuestions: [],
             currentQuestion: null,
             currentChoices: [],
@@ -23,71 +23,103 @@ class App extends React.Component {
     }
 
     componentWillMount() {
-        this.setState({
-            quizQuestions: randomize(quizQuestions)
-        })
+        if (quizQuestions) {
+            this.setState({
+                quizQuestions: randomize(quizQuestions)
+            })
+        }
     }
 
     componentDidMount() {
         if (this.state.quizQuestions) {
-            let question = this.state.quizQuestions.shift();
+            let singleQuestion = single(this.state.quizQuestions);
+            console.log(singleQuestion)
             this.setState({
-                question: this.state.question + 1,
-                currentQuestion: question.question,
-                currentChoices: question.choices,
-                correctChoice: question.correct
+                currentQuestion: singleQuestion.question,
+                currentChoices: singleQuestion.choices,
+                correctChoice: singleQuestion.correct
+            })
+        }
+    }
+
+    getQuestion() {
+        if (this.state.quizQuestions) {
+            let singleQuestion = single(this.state.quizQuestions);
+            this.setState({
+                questionCount: this.state.questionCount + 1,
+                currentQuestion: singleQuestion.question,
+                currentChoices: singleQuestion.choices,
+                correctChoice: singleQuestion.correct
+            })
+        }
+    }
+
+    nextQuestion() {
+        if (this.state.quizQuestions) {
+            let singleQuestion = single(this.state.quizQuestions);
+            this.setState({
+                questionCount: this.state.questionCount + 1,
+                currentQuestion: singleQuestion.question,
+                currentChoices: singleQuestion.choices,
+                correctChoice: singleQuestion.correct
             })
         }
     }
 
     checkAnswer(e) {
-        let correctAnswer = this.state.correctChoice;
+        let button = document.getElementById('js-check');
         let selectedAnswer = document.getElementsByClassName('selected')[0];
-        if (selectedAnswer.id !== correctAnswer) {
-            selectedAnswer.className += ' incorrect';
+        if (!selectedAnswer) {
+            return false;
         } else {
-            selectedAnswer.className += ' correct';
-            this.setState({
-                score: this.state.score + 1
-            })
+            let correctAnswer = this.state.correctChoice;
+            if (selectedAnswer.id !== correctAnswer) {
+                selectedAnswer.className += ' incorrect';
+                button.innerHTML = 'Next Question';
+            } else {
+                selectedAnswer.className += ' correct';
+                button.innerHTML = 'Next Question';
+                this.setState({
+                    userScore: this.state.userScore + 1
+                })
+            }
         }
     }
 
     startQuiz() {
         const showTopBar = document.getElementsByClassName('hide-show')[0];
-        showTopBar.style.opacity = 1; 
-
+        showTopBar.style.opacity = 1;
         this.setState({
-            intro: false
+            isIntro: false,
+            questionCount: this.state.questionCount + 1
         });
     }
 
     render() {
         const questions = this.state.quizQuestions;
         if (questions.length) {
-            console.log(true)
-        }
-        
-        return (
-            <div className="container page">
-                <div className="row hide-show">
-                    <QuestionCount question={this.state.question} />
-                    <ScoreCount score={this.state.score} />
-                </div>
-                <div className="row justify-content-center text-center">
-                    <div className="col-sm-12">
-                        <Content
-                            trigger={this.startQuiz}
-                            intro={this.state.intro}
-                            question={this.state.currentQuestion}
-                            choices={this.state.currentChoices}
-                            correct={this.state.correctChoice}
-                            check={this.checkAnswer}
-                        />
+            return (
+                <div className="container page">
+                    <div className="row hide-show">
+                        <QuestionCount question={this.state.questionCount} />
+                        <ScoreCount score={this.state.userScore} />
+                    </div>
+                    <div className="row justify-content-center text-center">
+                        <div className="col-sm-12">
+                            <Content
+                                intro={this.state.isIntro}
+                                trigger={this.startQuiz}
+                                count={this.state.questionCount}
+                                question={this.state.currentQuestion}
+                                choices={this.state.currentChoices}
+                                correct={this.state.correctChoice}
+                                check={this.checkAnswer}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
--- src/components/Content.js
+++ src/components/Content.js
@@ -2,24 +2,26 @@ import React from 'react';
 import { selectItem } from '../components/Helpers';
 
 const Content = (props) => {
-    if (props.intro) {
+    if (!props.intro) {
         return (
             <React.Fragment>
-                <h1 className="intro">Do <span>You</span><br />Remember<br/><span>the 90's?</span></h1>
-                <button onClick={props.trigger} className="btn btn-lg btn-primary btn-block">Let's Dance</button>
-            </React.Fragment>
-        );
-    } else {
-        return (
-            <React.Fragment>
-                <h4 className="qcount-heading">Question 1</h4>
-                <h1>{props.question}</h1>
+                <h4 className="qcount-heading">Question {props.count}</h4>
+                <h1 className="content">{props.question}</h1>
                 <ul className="choices" onClick={(e) => selectItem(e)}>
                     {props.choices.map((choice, i) => {
                         return <li id={i} key={i}>{choice}</li>
                     })}
                 </ul>
-                <button onClick={props.check} className="btn btn-lg btn-primary btn-block">Check Answer</button>
+                <button onClick={props.check} className="btn btn-lg btn-primary btn-block" id="js-check">
+                Select Answer
+                </button>
+            </React.Fragment>
+        );
+    } else {
+        return (
+            <React.Fragment>
+                <h1 className="intro">Do You<br /><span>Remember</span><br/>the<span> 90's?</span></h1>
+                <button onClick={props.trigger} className="btn btn-lg btn-primary btn-block">Let's Dance</button>
             </React.Fragment>
         );
     }
