0881ca30c62453c9dfa0bb259887d1ba394d0193
Asif Kabani <asifkabani@gmail.com>
2018-09-30 20:21:56 -0500

    Made changes

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,34 +1,12 @@
 import React from 'react';
-import { Animated } from 'react-web-animation';
 
-class Intro extends React.Component {
-
-    getKeyFrames() {
-        return [
-            { transform: 'scale(0)',    opacity: 0 },
-            { transform: 'scale(1)',   opacity: 1 }
-        ];
-    }
-
-    getTiming( duration ) {
-        return {
-            duration,
-            easing: 'ease-in',
-            delay: 0,
-            iterations: 1,
-            direction: 'alternate',
-            fill: 'forwards'
-        };
-    }
-
-    render() {
-        return (
-            <Animated.div keyframes={this.getKeyFrames()}
-                timing={this.getTiming(500)}>
-                <h1 className="intro"><span>Do You Remember<br/>the 90's?</span></h1>
-            </Animated.div>
-        );
-    }
+const Intro = () => {
+    return (
+        <div className="intro">
+            <h1 className="intro">Do You Remember<br />the 90's?</h1>
+            <p>Take this quiz to see if you remember the good ol' days.</p>
+        </div>
+    );
 }
 
 export default Intro;
